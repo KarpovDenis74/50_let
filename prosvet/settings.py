1bdc944e31a9ee23c7f48aff6bbe5794b6f62a0d
KarpovDenis74 <fotokarpov@yandex.ru>
2024-07-08 23:57:12 +0300

    Изменены названия переменных окружения - важно при создании контейнера базы данных

--- prosvet/settings.py
+++ prosvet/settings.py
@@ -64,9 +64,9 @@ WSGI_APPLICATION = 'prosvet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql_psycopg2'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': os.getenv('POSTGRES_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', 5432),
     }
