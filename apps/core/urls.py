8c2d03bce209ace534637865633f4f9c14cae5c8
KarpovDenis74 <fotokarpov@yandex.ru>
2024-07-13 22:17:14 +0300

    flake 8

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from django.contrib.auth import views
 from apps.core.views import index
 
 
--- apps/events/views.py
+++ apps/events/views.py
@@ -4,9 +4,6 @@ from django.views.generic.base import TemplateView
 
 class EventsView(TemplateView):
     template_name = 'events/index.html'
-    
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-
-
-
